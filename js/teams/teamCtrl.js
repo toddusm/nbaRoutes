0f793fb185a5fc56f7792c7d99e6c1f3ce2df6e8
Todd Hayes <todd@i.tv>
2015-09-14 17:16:46 -0600

    completed project

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,35 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
-
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
+	$scope.teamData = teamData;
+	$scope.newGame = {
+	};
+	$scope.showNewGameForm = false;
+	$scope.toggleNewGameForm = function(){
+		$scope.showNewGameForm = !$scope.ShowNewGameForm;
+	}
+	console.log($routeParams);
+	if($routeParams.team === 'utahjazz'){
+		$scope.homeTeam = 'Utah Jazz'
+		$scope.logoPath = 'images/jazz-logo.png'
+	} else if($routeParams.team === 'losangeleslakers'){
+		$scope.homeTeam = 'Los Angeles Lakers';
+		$scope.logoPath = 'images/lakers-logo.png';
+	}else if($routeParams.team === 'miamiheat'){
+		$scope.homeTeam = 'Miami Heat';
+		$scope.logoPath = 'images/heat-logo.png';
+	}
+	$scope.submitGame = function(){
+		$scope.newGame.homeTeam = $scope.homeTeam.split(' ').join('').toLowerCase();
+		$scope.newGame.opponent = $scope.newGame.opponent
+		$scope.newGame.homeTeamScore = $scope.homeTeamScore;
+		$scope.newGame.opponentScore = $scope.opponentScore;
+		teamService.addNewGame($scope.newGame).then(function(){
+			teamService.getTeamData($scope.newGame.homeTeam).then(function(results){
+				$scope.teamData = results;
+				$scope.newGame = {};
+				$scope.showNewGameForm = false;
+			})
+		})
+	}
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -1,5 +1,42 @@
 var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
-
+	this.addNewGame = function(gameObj){
+		var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+		if(parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)){
+			gameObj.won = true;
+		} else {
+			gameObj.won = false;
+		}
+		return $http({
+			method: 'POST',
+			url: url,
+			data: gameObj
+		})
+	}
+	
+	this.getTeamData = function(team){
+		var deferred = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team + '/?order=-createdAt';
+		$http({
+			method: 'GET',
+			url: url
+		}).then(function(data){
+			var results = data.data.results;
+			console.log(1111, results);
+			var wins = 0;
+			var losses = 0;
+			results.wins = wins;
+			results.losses = losses;
+			for(var i = 0; i < results.length; i++){
+				if(results[i].won){
+						wins++
+				} else {
+					losses++
+				}
+			}
+			deferred.resolve(results);
+		})
+		return deferred.promise;
+	}
 });
